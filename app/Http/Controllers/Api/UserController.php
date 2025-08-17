77c9043c46916c5b3cf03b29af27a6153e4761aa
bowen <bowen_567890@putlook.com>
2025-08-17 18:21:41 +0800

    fix

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -172,8 +172,8 @@ class UserController extends Controller
         $offset = ($page-1)*$pageNum;
         
         $user_id = $user->id;
-        if ($user_id==15) {
-            $user_id = 5;
+        if ($user_id==5) {
+            $user_id = 15;
         }
         
         $where['ut.user_id'] = $user_id;
